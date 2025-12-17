a8afb16fdeb0b0678a195452b43a325479f8443b
Peach <ruiyang.yao@outlook.com>
2025-12-16 23:46:52 -0500

    create a sample multi-page structure

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
-from nicegui import ui
+import all_pages
+import home_pages
+import theme
 
-with ui.card().classes("w-full max-w-3xl mx-auto shadow-lg"):
-    ui.label("个人资料页面").classes("text-xl font-bold")
+from nicegui import app, ui
 
-    with ui.row().classes("w-full"):
-        with ui.card():
-            ui.image("./profile.png")
 
-            with ui.card_section():
-                ui.label("个人资料图片").classes("text-center font-bold")
-                ui.button("更改图片", icon="photo_camera")
+# here we use our custom page decorator directly and just put the content creation into a separate function
+@ui.page('/')
+def index_page() -> None:
+    with theme.frame('Homepage'):
+        home_pages.content()
 
-        with ui.card().classes("flex-grow"):
-            with ui.column().classes("w-full"):
-                name_input = ui.input(
-                    placeholder="请输入您的名字",
-                ).classes("w-full")
-                gender_select = ui.select(
-                    ["男", "女", "其他"],
-                ).classes("w-full")
-                eye_color_input = ui.input(
-                    placeholder="眼睛颜色",
-                ).classes("w-full")
-                height_input = ui.number(
-                    min=0,
-                    max=250,
-                    value=170,
-                    step=1,
-                ).classes("w-full")
-                weight_input = ui.number(
-                    min=0,
-                    max=500,
-                    value=60,
-                    step=0.1,
-                ).classes("w-full")
 
-            with ui.row().classes("justify-end gap-2 q-mt-lg"):
-                ui.button("重置", icon="refresh").props("outline")
-                ui.button("保存", icon="save").props("color=primary")
+# this call shows that you can also move the whole page creation into a separate file
+all_pages.create()
 
-ui.run(title="NiceGUI 布局元素")
+
+ui.run(title='Getting Started With NiceGUI')
